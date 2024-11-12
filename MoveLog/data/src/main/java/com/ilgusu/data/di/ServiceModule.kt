6f2b20437818d6d635c4311b229065c6fae8bb26
moondev03 <moondev03@gmail.com>
2024-11-13 02:01:59 +0900

    chore(Project): Optimize Imports

--- MoveLog/data/src/main/java/com/ilgusu/data/di/ServiceModule.kt
+++ MoveLog/data/src/main/java/com/ilgusu/data/di/ServiceModule.kt
@@ -1,10 +1,8 @@
 package com.ilgusu.data.di
 
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
