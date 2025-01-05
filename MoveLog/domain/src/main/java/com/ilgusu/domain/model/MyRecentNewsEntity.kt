b40a3297ce01aff4a8849ec98e2c4cd3bec40a47
shong9124 <162269186+shong9124@users.noreply.github.com>
2025-01-05 20:45:34 +0900

    [MOV-25] 홈 화면 작업 (#5)

---
+++ MoveLog/domain/src/main/java/com/ilgusu/domain/model/MyRecentNewsEntity.kt
@@ -0,0 +1,5 @@
+package com.ilgusu.domain.model
+
+data class MyRecentNewsEntity(
+    val id : String
+)
---
+++ MoveLog/presentation/src/main/java/com/ilgusu/presentation/view/home/RvMyRecentNewsAdapter.kt
@@ -0,0 +1,36 @@
+package com.ilgusu.presentation.view.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.ilgusu.domain.model.MyRecentNewsEntity
+import com.ilgusu.presentation.databinding.ItemMyRecentNewsRvBinding
+
+class RvMyRecentNewsAdapter : RecyclerView.Adapter<RvMyRecentNewsAdapter.ViewHolder>() {
+    var list = mutableListOf<MyRecentNewsEntity>()
+    inner class ViewHolder(private val binding: ItemMyRecentNewsRvBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item : MyRecentNewsEntity) {
+            Glide.with(binding.imgRv)
+                .load(item.id)
+                .transform(RoundedCorners(40))
+                .into(binding.imgRv)
+        }
+    }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val binding = ItemMyRecentNewsRvBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
