2139e93ee8cdcf5e046fdb71f9b34d764d93cc28
shong9124 <shifty393@gmail.com>
2025-01-06 02:09:00 +0900

    설정 화면 작업

---
+++ MoveLog/presentation/src/main/java/com/ilgusu/presentation/view/setting/SettingFragment.kt
@@ -0,0 +1,46 @@
+package com.ilgusu.presentation.view.setting
+
+import android.content.Intent
+import android.net.Uri
+import androidx.lifecycle.lifecycleScope
+import com.ilgusu.navigation.NavigationCommand
+import com.ilgusu.presentation.base.BaseFragment
+import com.ilgusu.presentation.databinding.FragmentSettingBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SettingFragment : BaseFragment<FragmentSettingBinding>() {
+
+    private val serviceUrl = "https://amusing-consonant-8d3.notion.site/172968a00fd180f09f15eea77cd519a8?pvs=4"
+    private val privacyUrl = "https://amusing-consonant-8d3.notion.site/16f968a00fd180d2b951d7d1c8604be1?pvs=4"
+    override fun initView() {
+
+    }
+    override fun initListener() {
+        super.initListener()
+
+        binding.btnBack.setOnClickListener {
+            lifecycleScope.launch {
+                navigationManager.navigate(NavigationCommand.Back)
+            }
+        }
+        binding.tvAboutService.setOnClickListener {
+            moveInternet(serviceUrl)
+        }
+        binding.tvAboutPrivacy.setOnClickListener {
+            moveInternet(privacyUrl)
+        }
+        binding.btnAboutService.setOnClickListener {
+            moveInternet(serviceUrl)
+        }
+        binding.btnAboutPrivacy.setOnClickListener {
+            moveInternet(privacyUrl)
+        }
+    }
+
+    private fun moveInternet(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
+    }
+}
