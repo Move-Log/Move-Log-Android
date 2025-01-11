884d17ac372430f6cb5e19ef53e0ed09a87bb89c
moondev03 <moondev03@gmail.com>
2025-01-12 01:13:49 +0900

    [DEPLOY] 비공개 테스트 - 3 (0.0.1)

--- MoveLog/app/build.gradle.kts
+++ MoveLog/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.ilgusu.movelog"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
