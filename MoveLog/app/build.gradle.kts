1b87c4c18cbc211b63917b79ca45c7ff016f4fa9
moondev03 <moondev03@gmail.com>
2025-02-05 18:07:30 +0900

    [DEPLOY] 1.0.2 (7)

--- MoveLog/app/build.gradle.kts
+++ MoveLog/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.ilgusu.movelog"
         minSdk = 29
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.1"
+        versionCode = 7
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
